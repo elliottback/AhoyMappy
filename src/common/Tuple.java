0bc22ed43312367bb0703c237656da0b2d5e6255
elliottback <elliottback@gmail.com>
2019-04-27 10:27:56 +0900

    initial classes for simple map

---
+++ src/common/Tuple.java
@@ -0,0 +1,63 @@
+package common;
+
+import java.util.Objects;
+
+/**
+ * Tuple class for storing key,value pairs
+ */
+public class Tuple <K,V> {
+    private K key;
+    private V value;
+
+    public Tuple(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    /**
+     * Equality is based on keys only
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tuple<?, ?> tuple = (Tuple<?, ?>) o;
+        return Objects.equals(key, tuple.key);
+    }
+
+    /**
+     * Hashcode is based on keys only
+     * @return
+     */
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(key);
+    }
+
+    @Override
+    public String toString() {
+        return "Tuple{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
