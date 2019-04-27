69739d27fc8faea118ff750bf141f818268a706d
elliottback <elliottback@gmail.com>
2019-04-27 10:33:13 +0900

    now it has backing and size, time to implement the put/get!

--- src/elliott/back/maps/SimpleFlatMap.java
+++ src/elliott/back/maps/SimpleFlatMap.java
@@ -1,5 +1,8 @@
 package elliott.back.maps;
 
+import common.Tuple;
+
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
@@ -9,16 +12,33 @@ import java.util.Set;
  * a flat array which is doubled every time it becomes full (on insert).
  *
  * Both inserts and lookups use probing in the array to find the next free slot.
+ *
+ * Limited to around MAX_INT entries because arrays are int-indexed in Java.
  */
 public class SimpleFlatMap <K,V> implements Map<K,V> {
+
+    private Tuple<K,V> [] backing;
+    private int currentSize = 0;
+
+    /**
+     * Default construct = initial 32 entries
+     */
+    public SimpleFlatMap() {
+        this(32);
+    }
+
+    public SimpleFlatMap( int initialSize ) {
+        backing = new Tuple [initialSize];
+    }
+
     @Override
     public int size() {
-        return 0;
+        return currentSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return currentSize == 0;
     }
 
     @Override
@@ -47,13 +67,14 @@ public class SimpleFlatMap <K,V> implements Map<K,V> {
     }
 
     @Override
-    public void putAll(Map<? extends K, ? extends V> m) {
-
+    public void putAll(Map<? extends K, ? extends V> map) {
+        for(K key : map.keySet() )
+            this.put(key, map.get(key));
     }
 
     @Override
     public void clear() {
-
+        Arrays.fill(this.backing, null);
     }
 
     @Override
